29ae0bc62492a1dd101824074a08771bcf1a9dd9
jbfermor <jbfermor@hotmail.com>
2022-11-13 14:15:48 +0100

    init

---
+++ lib/easy_calendar/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EasyCalendar
+  VERSION = "0.1.0"
+end
---
+++ spec/easy_calendar_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EasyCalendar do
+  it "has a version number" do
+    expect(EasyCalendar::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
